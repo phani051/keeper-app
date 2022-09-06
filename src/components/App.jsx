12314d2c6798b48e5ae57cb8430c73eee2e7790b
Phani051 <phani051@gmail.com>
2022-09-06 17:09:34 +0530

    Used props and map function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import Header from "./Header";
 import Footer from "./Footer";
-import Note  from "./Note";
+import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
     <div className="body">
       <Header />
-      <Note />
+      {notes.map(note => 
+         (
+          <Note key={note.key} title={note.title} content={note.content} />
+        )
+      )}
       <Footer />
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-function Note() {
+
+function Note(props) {
   return (
     <div className="note">
-      <h1>Note Title</h1>
-      <p>This is notes content</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
   );
 }
